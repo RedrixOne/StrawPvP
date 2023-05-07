e75741e4fa8ee69b294465863984b75615497b84
RedrixOne <88893528+RedrixOne@users.noreply.github.com>
2023-05-07 17:31:51 +0200

    Update SpawnCommand.java

--- src/main/java/com/redrixone/commands/SpawnCommand.java
+++ src/main/java/com/redrixone/commands/SpawnCommand.java
@@ -1,5 +1,6 @@
 package com.redrixone.commands;
 
+import com.redrixone.managers.CombatManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -7,15 +8,29 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class SpawnCommand implements CommandExecutor {
+
+    private CombatManager combatManager;
+    
+    public SpawnCommand(CombatManager combatManager) {
+        this.combatManager = combatManager;
+    }
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        Player player = (Player) sender;
+        UUID playerUUID = player.getUniqueId();
         if (!(sender instanceof Player))
             return false;
         else {
-            Location spawn = new Location(Bukkit.getWorld("world"), -8.500, 184, -24.500);
-            Player player = (Player) sender;
-            player.teleport(spawn);
+            if (combatManager.isInCombat(playerUUID)) {
+                Location spawn = new Location(Bukkit.getWorld("world"), -8.500, 184, -24.500);
+                player.teleport(spawn);
+            } else {
+                player.sendMessage("§cNon puoi eseguire comandi durante il combattimento");
+            }
         }
 
         return true;
